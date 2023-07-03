5cb4e839e518c63bc76cc9693e1a44c27754022d
khanhsonb1c1 <khanhsonb1c1@gmail.com>
2023-07-03 20:37:07 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,35 @@ function App() {
 
   const formatLine = (jsonString: string) => {
     const regex = /\[(.*?)]\((\/en)?(.*?)\)/;
+    // [(.*?)] : lấy phần trong của dấu [ ]
+    // (.*?) : lấy phần trong của dấu ()
+    // (\/en)?: tùy chọn (?: có hoặc không) nếu có sẽ bỏ chữ "en" đi.
+    // 
     const match = jsonString.match(regex);
 
     if (match) {
       const title = match[1];
+
+      let title_1; // đoạn nội dung hiển thị
+      let title_2; // phần trong dấu ngoặc ( có thể có hoặc không, vì 1 số line k có)
+
+      if (title) {
+        const regex = /(.*?)\((.*?)\)/; // lấy ra phần ngoài và trong dấu ngoặc
+        const match = title.match(regex);
+
+        if (match) {
+          title_1 = match[1]; // lấy phần ngoài
+          title_2 = match[2]; // lấy phần trong
+        } else {
+          title_1 = title;
+        }
+      }
+
       const link = match[3];
       const formatElement = (
         <li key={link}>
-          <a href={`https://igx.biz${link}`}>{title}</a>
+          <a href={`https://igx.biz${link}`}>{title_1}</a>
+          {title_2 ? `(${title_2})` : ""}
         </li>
       );
 
